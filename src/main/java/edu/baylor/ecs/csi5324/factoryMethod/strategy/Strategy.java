16fcb1659db0c6bd503d55e01388deb8e232e0b4
PratisthaS <pratisthashres@gmail.com>
2019-09-06 17:19:26 -0500

    change

--- src/main/java/edu/baylor/ecs/csi5324/factoryMethod/strategy/Strategy.java
+++ src/main/java/edu/baylor/ecs/csi5324/factoryMethod/strategy/Strategy.java
@@ -1,4 +1,22 @@
 package edu.baylor.ecs.csi5324.factoryMethod.strategy;
 
+import edu.baylor.ecs.csi5324.factoryMethod.store.Store;
+import edu.baylor.ecs.csi5324.factoryMethod.distributor.Distributor;
+
 public abstract class Strategy {
+
+    private void selectDistributorBasedOnRank(Store store) {
+
+        int index = 0;
+        double rank = 0;
+        for (int i = 0; i < store.getDistributorList().size(); i++) {
+            Distributor distributor = store.getDistributorList().get(i);
+            if (distributor.getRank() > rank) {
+                index = i;
+                rank = distributor.getRank();
+            }
+            System.out.println("* " + distributor.getClass().getSimpleName());
+        }
+        store.selectDistributor(index);
+    }
 }
