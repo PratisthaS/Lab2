73e8a8a5aa19f5ce3581124f9a19db8fb45be057
PratisthaS <pratisthashres@gmail.com>
2019-09-06 19:25:46 -0500

    rank

--- src/main/java/edu/baylor/ecs/csi5324/factoryMethod/strategy/impl/Rank.java
+++ src/main/java/edu/baylor/ecs/csi5324/factoryMethod/strategy/impl/Rank.java
@@ -1,4 +1,25 @@
 package edu.baylor.ecs.csi5324.factoryMethod.strategy.impl;
 
-public class Rank {
+import edu.baylor.ecs.csi5324.factoryMethod.distributor.Distributor;
+import edu.baylor.ecs.csi5324.factoryMethod.store.Store;
+import edu.baylor.ecs.csi5324.factoryMethod.strategy.Strategy;
+
+import java.util.List;
+
+
+public class Rank extends Strategy {
+
+    public void selectDistributor(Store store) {
+        int index = 0;
+        double rank = 0;
+        for (int i = 0; i < store.getDistributorList().size(); i++) {
+            Distributor distributor = store.getDistributorList().get(i);
+            if (distributor.getRank() > rank) {
+                index = i;
+                rank = distributor.getRank();
+            }
+            System.out.println("* " + distributor.getClass().getSimpleName());
+        }
+         store.selectDistributor(index);
+    }
 }
