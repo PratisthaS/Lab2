927c9f5f21f7b31354fec135269c4b221396aafe
PratisthaS <pratisthashres@gmail.com>
2019-09-06 19:26:08 -0500

    price

--- src/main/java/edu/baylor/ecs/csi5324/factoryMethod/strategy/impl/Price.java
+++ src/main/java/edu/baylor/ecs/csi5324/factoryMethod/strategy/impl/Price.java
@@ -1,4 +1,28 @@
 package edu.baylor.ecs.csi5324.factoryMethod.strategy.impl;
 
-public class Price {
+import edu.baylor.ecs.csi5324.factoryMethod.distributor.Distributor;
+import edu.baylor.ecs.csi5324.factoryMethod.store.Store;
+import edu.baylor.ecs.csi5324.factoryMethod.strategy.Strategy;
+
+import java.util.List;
+
+public class Price extends Strategy {
+
+
+    public void selectDistributor(Store store) {
+        int index = 0;
+        double charge = 0;
+        double distributorCharge;
+        for (int i = 0; i < store.getDistributorList().size(); i++) {
+            Distributor distributor = store.getDistributorList().get(i);
+             distributorCharge = distributor.getCharge().doubleValue();
+            if (distributorCharge > charge) {
+                index = i;
+                charge = distributorCharge;
+            }
+            System.out.println("* " + distributor.getClass().getSimpleName());
+        }
+        store.selectDistributor(index);
+    }
+
 }
